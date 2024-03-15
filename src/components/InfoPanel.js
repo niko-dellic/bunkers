d2baa7de303736117eb6035296d8820c8c2b4fe0
codeAndie <114886583+codeAndie@users.noreply.github.com>
2024-03-15 13:32:32 -0400

    Info UI
    Added image, font, changed some content and made some html/css changes

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -1,13 +1,32 @@
 export default function InfoPanel({ minesweeperBunkers }) {
   return (
-    <div>
-      <h2>Welcome to BunkerTown, MA</h2>
-      <p>
+    // <div>
+    //   <h2>Welcome to BunkerTown, MA</h2>
+    //   <p>
+    //     Population:
+    //     {minesweeperBunkers.length}
+    //   </p>
+    //   <p>Top items stockpiled:</p>
+    //   {/* TO DO - add gpt data looking at top items */}
+    // </div>
+
+    <div class="info">
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          src="/assets/img/smile.png"
+          alt="Description of image"
+          style={{ width: "50px", paddingTop: "10px" }}
+        />
+      </div>
+      <div id="info-title">Welcome to BunkerMatch</div>
+      <div id="population">
         Population:
         {minesweeperBunkers.length}
+      </div>
+
+      <p class="instructions">
+        Browse the map and find a place to create your new emergency shelter.
       </p>
-      <p>Top items stockpiled:</p>
-      {/* TO DO - add gpt data looking at top items */}
     </div>
   );
 }
--- src/components/cass.js
+++ src/components/cass.js
@@ -154,3 +154,48 @@ export default function UX({
     </>
   );
 }
+
+
+
+
+import OpenAI from "openai";
+import { useEffect } from "react";
+
+const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+export default function GenBunker({ userString, result, setResult }) {
+  //   const [generatedText, setGeneratedText] = useState('');
+
+  useEffect(() => {
+    console.log("use effect called", userString);
+    if (!userString) return;
+
+    async function genContent(prompt) {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt }],
+        model: "gpt-3.5-turbo",
+      });
+      setResult(completion.choices[0].message.content);
+    }
+
+    async function genImage(prompt) {
+      const image = await openai.images.generate({
+        prompt: prompt,
+        size: "256x256",
+        quality: "standard",
+        n: 1,
+      });
+      const imgUrl = image.data[0].url;
+      console.log(image.data[0].url);
+    }
+
+    const text = genContent(userString);
+    genImage("happy cats and dogs");
+  }, [userString]);
+}
+
